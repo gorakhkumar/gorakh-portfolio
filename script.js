65d5030d5b700146eea1074c4d36c722b4ed5c00
Gorakh Kumar <gorakhkumar.dev@gmail.com>
2026-01-29 22:11:17 +0530

    upload my portfolio

---
+++ script.js
@@ -0,0 +1,31 @@
+const navbar = document.getElementById("navbar");
+const navLink = document.getElementById("navLink");
+const mobileMenu = document.getElementById("mobileMenu");
+
+function openMenu() {
+    mobileMenu.style.transform = 'translateX(-16rem)';
+}
+
+function closeMenu() {
+    mobileMenu.style.transform = 'translateX(0)';
+}
+
+function toggleTheme() {
+    document.documentElement.classList.toggle('dark');
+
+    if (document.documentElement.classList.contains('dark')) {
+        localStorage.theme = 'dark';
+    } else {
+        localStorage.theme = 'light';
+    }
+}
+
+window.addEventListener('scroll', () => {
+    if (scrollY > 50) {
+        navbar.classList.add('bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm',);
+        navLink.classList.remove('bg-white', 'shadow-sm', 'bg-opacity-50', 'dark:border', 'dark:border-white/30', "dark:bg-transparent");
+    } else {
+        navbar.classList.remove('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm', 'dark:bg-darkTheme', 'dark:shadow-white/20');
+        navLink.classList.add('bg-white', 'shadow-sm', 'bg-opacity-50', 'dark:border', 'dark:border-white/30', "dark:bg-transparent");
+    }
+})
